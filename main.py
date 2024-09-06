079e9c57ec1cd37a13bc4b2230c2764ed0ce30cd
vitorlany <vitorlany99@gmail.com>
2024-09-06 18:25:10 -0300

    feat: get java repos

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from get_data import get_popular_java_repos
 
 if __name__ == "__main__":
-    num_repos = 10
+    num_repos = 1000
     try:
         popular_repos = get_popular_java_repos(num_repos)
+        print(popular_repos)
     except Exception as e:
         print(e)
     exit(0)
