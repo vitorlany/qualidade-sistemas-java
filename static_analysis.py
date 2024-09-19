fec50852e3c79b52e9fa55901983d1291bca6a20
Leticia <leticia.fraga@epitech.eu>
2024-09-19 18:05:09 -0300

    feat: delete clone after getting metrics

--- static_analysis.py
+++ static_analysis.py
@@ -5,30 +5,37 @@ CK_ANALYSIS_PATH = "./ck_results"
 CK_PATH = "../ck/target/ck-0.7.1-SNAPSHOT-jar-with-dependencies.jar"
 
 def get_metrics(popular_repos):
-    clone_repos(popular_repos)
-    run_ck(popular_repos)
+    subprocess.run(["mkdir", CLONED_REPOS_PATH])
+    subprocess.run(["mkdir", CK_ANALYSIS_PATH])
+    for repo in popular_repos:
+        repo_name = repo["node"]["name"]
+        clone_repos(repo_name, repo["node"]["url"])
+        run_ck(repo_name)
+        delete_clone(repo_name)
 
 # Clonar repos para pasta ./repos
-def clone_repos(popular_repos):
-    for repo in popular_repos:
-        name = repo["node"]["name"]
-        url = repo["node"]["url"]
-        repo_path =  CLONED_REPOS_PATH + "/" + name
+def clone_repos(name, url):
+    repo_path =  CLONED_REPOS_PATH + "/" + name
 
-        print(repo_path)
+    print(repo_path)
 
-        subprocess.run(["git", "clone", url, repo_path])
+    subprocess.run(["git", "clone", url, repo_path])
+
+def delete_clone(name):
+    repo_path =  CLONED_REPOS_PATH + "/" + name
+
+    print(repo_path)
+
+    subprocess.run(["rm", "-rf", repo_path])
 
 # Executar análise estática
-def run_ck(popular_repos):
+def run_ck(repo_name):
     subprocess.run(["mkdir", CK_ANALYSIS_PATH])
 
-    for repo in popular_repos:
-        name = repo["node"]["name"]
-        repo_path =  CLONED_REPOS_PATH + "/" + name
-        results_path =  CK_ANALYSIS_PATH + "/" + name + "/"
+    repo_path =  CLONED_REPOS_PATH + "/" + repo_name
+    results_path =  CK_ANALYSIS_PATH + "/" + repo_name + "/"
 
-        print(results_path)
+    print(results_path)
 
-        subprocess.run(["mkdir", results_path])
-        subprocess.run(["java", "-jar", CK_PATH, repo_path, "true", "0", "false", results_path])
+    subprocess.run(["mkdir", results_path])
+    subprocess.run(["java", "-jar", CK_PATH, repo_path, "true", "0", "true", results_path])
